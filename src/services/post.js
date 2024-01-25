f8551174ba208541e1f128c06a5b88554f5c5385
leonazraev <33936664+leonazraev@users.noreply.github.com>
2024-01-26 01:52:36 +0200

    Update post.js

--- src/services/post.js
+++ src/services/post.js
@@ -1,9 +1,17 @@
 import PostModel from "../models/post.js";
 
+//optinal: Typed return?
+// I don't like the way You use the functions names, but because it's service I can accept that because the destructing
+//why not create(newPost) and when you import this js file you use postsService.create(post) I think it's much cleaner.
+//same for the second service
+
 
 export async function createPostService(createPost) {
    const post = new PostModel(createPost)
     await post.save()
+   //I think it's better to return the post.
+   //Maybe next someone will make some manipulation on the result?
+   //It's not must but it's cleaner
 }
 
 export async function getPostsService({start, limit}) {
@@ -11,6 +19,7 @@ export async function getPostsService({start, limit}) {
 }
 
 export async function getPostsAmountService() {
+    // totalPosts()
     return PostModel.countDocuments().exec();
 }
 
