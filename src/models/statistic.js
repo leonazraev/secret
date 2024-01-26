e9fb7e3ab9bff115df3f2be73196fe1708278ddb
leonazraev <33936664+leonazraev@users.noreply.github.com>
2024-01-26 02:14:52 +0200

    Update statistic.js

--- src/models/statistic.js
+++ src/models/statistic.js
@@ -15,9 +15,9 @@ const StatisticSchema = new mongoose.Schema({
         default: 0
     }
     //Are you sure about this approach?
-    //They ask for runtime avg of the 2 functions, I think you only need a name and runtime
+    //They ask for runtime avg of the 2 functions, I think you only need a name and runtime then in the service calculate it by query group
     //It can be a good question to ask, because it's statistics maybe they want to analyze this data by days? years?
-    //If it does, again think about index to name.
+    //If it does, again think about indexes.
     //timestamps
 });
 
